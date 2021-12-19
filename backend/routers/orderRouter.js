86fe8d09552e6f8686bd03f13ca1e379c7d2e27c
Hafsa-Begum <hafsagood@gmail.com>
2021-12-19 13:02:48 +0600

    created-api-for-single-order

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -30,4 +30,18 @@ orderRouter.post(
     })
 );
 
+//to get a single order
+orderRouter.get(
+    '/:id',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            res.send(order);
+        } else {
+            res.status(404).send({ message: 'Order Not Found' });
+        }
+    })
+);
+
 export default orderRouter;
