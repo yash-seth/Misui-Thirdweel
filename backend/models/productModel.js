4e93b83f737eac831ecb945e4faf3d62838b36cd
Hafsa-Begum <hafsagood@gmail.com>
2021-12-19 12:56:55 +0600

    image-price-property-added

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,13 +4,15 @@ const productSchema = new mongoose.Schema(
     {
         name: { type: String, required: true, unique: true },
         kind: { type: String, required: true },
-        brand: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: String, required: true },
         category: { type: String, required: true },
         subCategory: { type: String, required: true },
         description: { type: String, required: true },
         size: { type: String, required: true },
         color: { type: String, required: true },
         tags: { type: String, required: true },
+        brand: { type: String, required: true },
     },
     {
         timestamps: true,
