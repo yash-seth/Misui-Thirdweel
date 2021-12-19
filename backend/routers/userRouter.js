f1731093db916de9c6ff17c9e4ab96e528009d6f
Hafsa-Begum <hafsagood@gmail.com>
2021-12-19 15:56:39 +0600

    created-api-to-update-user-details

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from "express-async-handler";
 import bcrypt from 'bcryptjs';
 import data from "../data.js";
 import User from "../models/userModel.js";
-import { generateToken } from "../utils.js";
+import { generateToken, isAuth } from "../utils.js";
 
 const userRouter = express.Router();
 
@@ -64,7 +64,7 @@ userRouter.post(
     })
 );
 
-//get one user details
+//get single user details
 userRouter.get(
     '/:id',
     expressAsyncHandler(async (req, res) => {
@@ -73,6 +73,33 @@ userRouter.get(
             res.send(user);
         } else {
             res.status(404).send({ message: 'User Not Found' });
+        }
+    })
+);
+
+//update user details
+userRouter.put(
+    '/profile',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            if (req.body.password) {
+                user.password = bcrypt.hashSync(req.body.password, 8);
+            }
+            const updatedUser = await user.save();
+            res.send({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                age: updatedUser.age,
+                gender: updatedUser.gender,
+                phone: updatedUser.phone,
+                email: updatedUser.email,
+                isAdmin: updatedUser.isAdmin,
+                token: generateToken(updatedUser),
+            });
         }
     })
 );
