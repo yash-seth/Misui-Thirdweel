f89e8578dbd4aa42debcba48f5cb9210ff09e696
Hafsa-Begum <hafsagood@gmail.com>
2021-12-19 16:53:51 +0600

    post-api-for-creating-new-product

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js'
 import Product from '../models/productModel.js';
+import { isAuth, isAdmin } from '../utils.js';
 
 const productRouter = express.Router();
 
@@ -27,5 +28,32 @@ productRouter.get('/:id', expressAsyncHandler(async (req, res) => {
     }
 
 }));
+
+//to create new product
+productRouter.post(
+    '/',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+        const product = new Product({
+            name: req.body.name,
+            kind: req.body.kind,
+            image: req.body.image,
+            price: req.body.price,
+            category: req.body.category,
+            subCategory: req.body.subCategory,
+            description: req.body.description,
+            size: req.body.size,
+            color: req.body.color,
+            tags: req.body.tags,
+            brand: req.body.brand,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+        });
+        const createdProduct = await product.save();
+        res.send({ message: 'Product Created', product: createdProduct });
+    })
+);
 
 export default productRouter;
