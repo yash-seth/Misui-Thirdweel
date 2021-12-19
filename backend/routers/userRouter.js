aa7a4f224a39666fa34566bc46048e72118879c2
Hafsa-Begum <hafsagood@gmail.com>
2021-12-19 11:42:08 +0600

    user-signin-updated-register-completed

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -14,6 +14,7 @@ userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
     res.send(createdUsers);
 }));
 
+//user signin
 userRouter.post(
     '/signin',
     expressAsyncHandler(async (req, res) => {
@@ -23,6 +24,9 @@ userRouter.post(
                 res.send({
                     _id: user._id,
                     name: user.name,
+                    age: user.age,
+                    gender: user.gender,
+                    phone: user.phone,
                     email: user.email,
                     isAdmin: user.isAdmin,
                     token: generateToken(user),
@@ -31,6 +35,32 @@ userRouter.post(
             }
         }
         res.status(401).send({ message: 'Invalid email or password' });
+    })
+);
+
+//user register
+userRouter.post(
+    '/register',
+    expressAsyncHandler(async (req, res) => {
+        const user = new User({
+            name: req.body.name,
+            age: req.body.age,
+            gender: req.body.gender,
+            phone: req.body.phone,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 8),
+        });
+        const createdUser = await user.save();
+        res.send({
+            _id: createdUser._id,
+            name: createdUser.name,
+            age: createdUser.age,
+            gender: createdUser.gender,
+            phone: createdUser.phone,
+            email: createdUser.email,
+            isAdmin: createdUser.isAdmin,
+            token: generateToken(createdUser),
+        });
     })
 );
 
