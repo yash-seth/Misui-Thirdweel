9b6caa69ca78f7ee8b1cabef4e70db1fd6d92e5c
Hafsa-Begum <hafsagood@gmail.com>
2021-12-19 16:22:53 +0600

    edited-product-schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,6 +13,9 @@ const productSchema = new mongoose.Schema(
         color: { type: String, required: true },
         tags: { type: String, required: true },
         brand: { type: String, required: true },
+        countInStock: { type: Number, required: true },
+        rating: { type: Number, required: true },
+        numReviews: { type: Number, required: true },
     },
     {
         timestamps: true,
