3cc448a8a52d7d12d7188e0e2dcef3196405d1ef
Hafsa-Begum <hafsagood@gmail.com>
2022-01-12 22:13:19 +0600

    product-service-completed

---
+++ backend/productService/productData.js
@@ -0,0 +1,18 @@
+
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            description: 'high quality product',
+            unitPrice: 120,
+            discount: 5,
+            unitsInStock: 10,
+            rating: 4.5,
+            size: 's'
+
+        },
+
+    ],
+}
+
+module.exports = data;
