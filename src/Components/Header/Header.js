21cfde543f642622c136986a533dd4cb2ead708b
Yash Seth <yashseth2002@gmail.com>
2022-06-10 14:17:39 +0530

    added header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+        <div className="main">
+            <div className="company-info">
+                <div className="logo"><img src="http://misui.in/media/logo.svg" alt="company-logo" height="25px" /></div>
+                <div className="logo_text"><b>Thirdweel</b></div>
+            </div>
+            <div className='buttons'>
+                <button id='Home'>Home</button>
+                <button id='About'>About Us</button>
+                <button id='Hiring'>Hiring</button>
+                <button id='Contact'>Contact Us</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
