6d571234bd9e341be1832442c41eacc47ac393dc
Yash Seth <yashseth2002@gmail.com>
2022-06-16 17:30:17 +0530

    added footer code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Footer/>
+    </>
   );
 }
 
