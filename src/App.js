2dd975bd5ea2ea948ea1d8f5735051dd65a34704
Yash Seth <yashseth2002@gmail.com>
2022-06-27 10:43:37 +0530

    added code cleanup

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <Route path="/MyCart" element={<MyCart />} />
         <Route path="/" element={<>
         <Header />
-        <HeaderMenu menuState = {menuState} setMenuState={setMenuState}/>
+        <HeaderMenu />
         <Stories/>
         <DiscountBar/>
         <TopOffers/>
