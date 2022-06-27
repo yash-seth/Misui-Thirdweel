2ad6771af7a0e8ba937ce2e7d06d53b4f1c60248
Yash Seth <yashseth2002@gmail.com>
2022-06-27 10:53:32 +0530

    added code improvements

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,8 +9,8 @@ function Footer() {
             <p id="sub-footerHeading">We provide the best in the class e-commerce service, we are always there for you</p>
             <b id="connectText">Where ever you are.</b>
             <hr />
-            <div class="footerRow">
-                <div class="footerColumnLink">
+            <div className="footerRow">
+                <div className="footerColumnLink">
                     <p className="list-header"><b>Links</b></p>
                     <div className='list-items'>
                         <div className='list-item'><img className="link-icon" src={require("./links.png")} alt="link icon" />Home</div><br/>
@@ -19,14 +19,14 @@ function Footer() {
                         <div className='list-item'><img className="link-icon" src={require("./links.png")} alt="link icon" />Contact Us</div>
                     </div>
                 </div>
-                <div class="footerColumn">
+                <div className="footerColumn">
                 <p className="list-header"><b>Contact</b></p>
                     <div className='list-items'>
                         <div className='list-item'><img className="link-icon" src={require("./call.png")} alt="call icon" />919191919191</div><br/>
                         <div className='list-item'><img className="link-icon" src={require("./location.png")} alt="location icon" />2nd floor, K-tech NASSCOM, 67, Phase 3, 1st Sector, HSR Layout, Bengaluru, Karnataka 560102</div>
                     </div>
                 </div>
-                <div class="footerColumn">
+                <div className="footerColumn">
                 <p className="list-header"><b>Legals</b></p>
                     <div className='list-items'>
                         <div className='list-item'><img className="link-icon" src={require("./legal.png")} alt="legal icon" />Terms of use</div><br />
